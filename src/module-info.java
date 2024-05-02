9dc6a3a8825fa125f103d434a0c788518d42c6c2
miravetejuanm <juanmiravete@hotmail.es>
2024-05-03 00:12:46 +0200

    Subida de los objetos

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MPCJUAN_ACTIVIDAD3 {
+}
---
+++ src/testing/ImprimirNombre.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class ImprimirNombre {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hola me llamo Juan");
+		
+	}
+
+}
